fab237d2c2d9a1425889ee72d9d5bb25bce5db02
Bob Qi <baobin.qi@gmail.com>
2024-05-30 16:37:23 +0800

    remove ArenaAllocator usages

--- src/main.zig
+++ src/main.zig
@@ -2,30 +2,37 @@ const std = @import("std");
 const zap = @import("zap");
 const mem = std.mem;
 const fmt = std.fmt;
+const ArrayList = std.ArrayList;
 
 const Team = struct {
     const Self = @This();
 
     allocator: mem.Allocator,
-    arena: std.heap.ArenaAllocator,
     name: []const u8,
+    cache: std.ArrayList([]const u8),
     packages: std.ArrayList([]const u8),
 
-    pub fn init(child_allocator: mem.Allocator, name: []const u8) Self {
-        var arena = std.heap.ArenaAllocator.init(child_allocator);
-        const allocator = arena.allocator();
+    pub fn init(allocator: mem.Allocator, name: []const u8) Self {
         return .{
             .allocator = allocator,
-            .arena = arena,
             .name = allocator.dupe(u8, name) catch "",
-            .packages = std.ArrayList([]const u8).init(child_allocator),
+            .cache = ArrayList([]const u8).init(allocator),
+            .packages = ArrayList([]const u8).init(allocator),
         };
     }
 
     pub fn deinit(self: *Self) void {
-        self.allocator.free(self.name);
         self.packages.deinit();
-        self.arena.deinit();
+        for (self.cache.items) |c| {
+            self.allocator.free(c);
+        }
+        self.cache.deinit();
+    }
+
+    pub fn readFile(self: *Self, file: std.fs.File) []const u8 {
+        const content = std.fs.File.readToEndAlloc(file, self.allocator, @as(usize, 1_000_000)) catch return "";
+        self.cache.append(content) catch return content;
+        return content;
     }
 
     pub fn containsPackage(self: *const Self, package: []const u8) bool {
@@ -46,10 +53,17 @@ pub const Handler = struct {
     pub fn init(allocator: mem.Allocator) Self {
         var self = Self{
             .allocator = allocator,
-            .teams = std.ArrayList(Team).init(allocator),
+            .teams = ArrayList(Team).init(allocator),
         };
         self.refresh();
         return self;
+    }
+
+    fn deinit_teams(teams: *ArrayList(Team)) void {
+        for (teams.items) |*t| {
+            t.deinit();
+        }
+        teams.deinit();
     }
 
     pub fn deinit(self: *Self) void {
@@ -64,7 +78,7 @@ pub const Handler = struct {
             var file = dir.openFile(entry.name, .{}) catch continue;
             defer file.close();
             var team = Team.init(self.allocator, entry.name);
-            const content = std.fs.File.readToEndAlloc(file, team.allocator, @as(usize, 100_000_000)) catch continue;
+            const content = team.readFile(file);
             var lines = mem.tokenize(u8, content, "\n");
             while (lines.next()) |line| {
                 team.packages.append(line) catch break;
@@ -77,17 +91,28 @@ pub const Handler = struct {
     }
 
     pub fn on_request(self: *Self, r: zap.Request) void {
-        var arena = std.heap.ArenaAllocator.init(self.allocator);
-        defer arena.deinit();
-        const allocator = arena.allocator();
-
-        var output: []const u8 = undefined;
         if (r.getParamSlice("q")) |the_query| {
+            const allocator = self.allocator;
+
             var elements = std.ArrayList([]const u8).init(allocator);
+            defer {
+                for (elements.items) |e| {
+                    allocator.free(e);
+                }
+                elements.deinit();
+            }
+
             var iter = mem.splitScalar(u8, the_query, '\n');
             while (iter.next()) |element| {
                 const package = get_package_name(element);
                 var teams = std.ArrayList([]const u8).init(allocator);
+                defer {
+                    for (teams.items) |s| {
+                        allocator.free(s);
+                    }
+                    teams.deinit();
+                }
+
                 for (self.teams.items) |t| {
                     if (t.containsPackage(package)) {
                         teams.append(fmt.allocPrint(allocator, "<a href=''>{s}</a>", .{t.name}) catch break) catch break;
@@ -95,11 +120,14 @@ pub const Handler = struct {
                 }
                 elements.append(fmt.allocPrint(allocator, "{s} -- {s}", .{ package, mem.join(allocator, "", teams.items) catch "" }) catch break) catch break;
             }
-            output = mem.join(allocator, "\n", elements.items) catch "OOM";
+            const output = mem.join(allocator, "\n", elements.items) catch "OOM";
+            defer allocator.free(output);
+            const body = fmt.allocPrint(allocator, "<html><body><h1>{s}</h1></body></html>", .{output}) catch return;
+            defer allocator.free(body);
+            r.sendBody(body) catch return;
         } else {
-            output = "Hello!";
+            r.sendBody("<html><body><h1>Hello!</h1></body></html>") catch return;
         }
-        r.sendBody(fmt.allocPrint(allocator, "<html><body><h1>{s}</h1></body></html>", .{output}) catch return) catch return;
     }
 };
 
@@ -114,30 +142,32 @@ pub fn main() !void {
     const allocator = gpa.allocator();
     defer std.debug.assert(gpa.deinit() == .ok);
 
-    var router = zap.Router.init(allocator, .{
-        .not_found = not_found,
-    });
-    defer router.deinit();
+    {
+        var router = zap.Router.init(allocator, .{
+            .not_found = not_found,
+        });
+        defer router.deinit();
 
-    var handler = Handler.init(allocator);
-    defer handler.deinit();
-    try router.handle_func("/", &handler, &Handler.on_request);
-    try router.handle_func("/init", &handler, &Handler.refresh);
+        var handler = Handler.init(allocator);
+        defer handler.deinit();
+        try router.handle_func("/", &handler, &Handler.on_request);
+        try router.handle_func("/init", &handler, &Handler.refresh);
 
-    var listener = zap.HttpListener.init(.{
-        .port = 9000,
-        .on_request = router.on_request_handler(),
-        .log = true,
-    });
-    try listener.listen();
+        var listener = zap.HttpListener.init(.{
+            .port = 9000,
+            .on_request = router.on_request_handler(),
+            .log = true,
+        });
+        try listener.listen();
 
-    std.debug.print("Listening on 0.0.0.0:9000\n", .{});
+        std.debug.print("Listening on 0.0.0.0:9000\n", .{});
 
-    // start worker threads
-    zap.start(.{
-        .threads = 2,
-        .workers = 1,
-    });
+        // start worker threads
+        zap.start(.{
+            .threads = 2,
+            .workers = 1,
+        });
+    }
 }
 
 fn get_package_name(trace: []const u8) []const u8 {
@@ -150,13 +180,6 @@ fn get_package_name(trace: []const u8) []const u8 {
         package = trace[0..j];
     }
     return package;
-}
-
-fn deinit_teams(list: *std.ArrayList(Team)) void {
-    for (list.items) |*t| {
-        t.deinit();
-    }
-    list.deinit();
 }
 
 test "test package name extraction" {
